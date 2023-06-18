4b9d4ea78f05a8cb8026fb84b31b190b8aec7d3f
Rens Arts <rens747@gmail.com>
2023-06-19 00:21:10 +0200

    music and sfx buttons stay in their chosen state after reloading game

--- dist/flappybird.js
+++ dist/flappybird.js
@@ -92,7 +92,7 @@ if (savedResolution) {
       canvas.width = 1280;
       canvas.height = 540;
       resolutionAdjust = 2;
-      secondResolutionAdjust = 1.5;
+      secondResolutionAdjust = 1.4;
       break;
     case "0.25":
       canvas.width = 640;
@@ -893,25 +893,41 @@ function createMusicButton() {
   button.style.width = Math.ceil(canvas.width * resolutionAdjust / 32) + "px";
   button.style.height = Math.ceil(canvas.width * resolutionAdjust / 32) + "px";
 
+  // Retrieve the previous state from local storage or default to true
+  var isMusicOn = localStorage.getItem("isMusicOn") === "true";
+
+  // Set the initial button appearance based on the state
+  setButtonAppearance();
+
   // Add event listener to handle button click
   button.addEventListener("click", function() {
     buttonSound.play();
+    isMusicOn = !isMusicOn; // Toggle the state
+    setButtonAppearance(); // Update the button appearance
+
+    // Store the updated state in local storage
+    localStorage.setItem("isMusicOn", isMusicOn.toString());
+  });
+
+  // Function to update the button appearance based on the state
+  function setButtonAppearance() {
     if (isMusicOn) {
-      // Pause the background music
-      backgroundMusic.volume = 0;
-      isMusicOn = false;
-      button.style.backgroundImage = "url('assets/musicOffButton.png')";
-    } else {
+      button.style.backgroundImage = "url('assets/musicButton.png')";
       // Resume playing the background music
       backgroundMusic.volume = 1;
-      isMusicOn = true;
-      button.style.backgroundImage = "url('assets/musicButton.png')";
+    } else {
+      button.style.backgroundImage = "url('assets/musicOffButton.png')";
+      // Pause the background music
+      backgroundMusic.volume = 0;
     }
-  });
+  }
 
   // Append the button to the body element
   document.body.appendChild(button);
 }
+
+createMusicButton();
+
 
 // Creates the button to turn the sound effects on or off
 function createSfxButton() {
@@ -922,52 +938,73 @@ function createSfxButton() {
   button.style.width = Math.ceil(canvas.width * resolutionAdjust / 32) + "px";
   button.style.height = Math.ceil(canvas.width * resolutionAdjust / 32) + "px";
 
+  // Retrieve the previous state from local storage or default to true
+  var isSfxOn = localStorage.getItem("isSfxOn") === "true";
+
+  // Set the initial button appearance based on the state
+  setButtonAppearance();
+
   // Add event listener to handle button click
   button.addEventListener("click", function() {
     buttonSound.play();
     initializeFlapSounds();
     initializeCoinSounds();
-    if (isSfxOn) {
-      // Disable the SFX
-      isSfxOn = false;
-      button.style.backgroundImage = "url('assets/sfxOffButton.png')";
-      for (var i = 0; i < flapSounds.length; i++) {
-        flapSounds[i].volume = 0;
-      }
-      for (var i = 0; i < coinSounds.length; i++) {
-        coinSounds[i].volume = 0;
-      }
-      for (var i = 0; i < coinSounds.length; i++) {
-        coinSounds[i].volume = 0;
-      }
-      deathSound.volume = 0;
-      startSound.volume = 0;
-      ghostSound.volume = 0;
-      sizeSound.volume = 0;
-      reduceGapSound.volume = 0;
-    } else {
-      // Enable the SFX
-      isSfxOn = true;
-      button.style.backgroundImage = "url('assets/sfxButton.png')";
-      for (var i = 0; i < flapSounds.length; i++) {
-        flapSounds[i].volume = 1;
-      }
-      for (var i = 0; i < coinSounds.length; i++) {
-        coinSounds[i].volume = 1;
-      }
-      for (var i = 0; i < coinSounds.length; i++) {
-        coinSounds[i].volume = 1;
-      }
-      deathSound.volume = 1;
-      startSound.volume = 1;
-      ghostSound.volume = 1;
-      sizeSound.volume =  1;
-      reduceGapSound.volume = 1;
-    }
+    isSfxOn = !isSfxOn; // Toggle the state
+    setButtonAppearance(); // Update the button appearance
+
+    // Store the updated state in local storage
+    localStorage.setItem("isSfxOn", isSfxOn.toString());
+
+    // Adjust volume based on the state
+    adjustVolume();
   });
+
+  // Function to update the button appearance based on the state
+  function setButtonAppearance() {
+    if (isSfxOn) {
+      button.style.backgroundImage = "url('assets/sfxButton.png')";
+    } else {
+      button.style.backgroundImage = "url('assets/sfxOffButton.png')";
+    }
+  }
+
+  // Function to initialize or update the volume of flapSounds and coinSounds based on the state
+  function initializeSoundVolume(soundArray, volume) {
+    for (var i = 0; i < soundArray.length; i++) {
+      soundArray[i].volume = volume;
+    }
+  }
+
+  // Function to adjust the volume of sound effects based on the state
+  function adjustVolume() {
+    var volume = isSfxOn ? 1 : 0;
+    initializeSoundVolume(flapSounds, volume);
+    initializeSoundVolume(coinSounds, volume);
+    deathSound.volume = volume;
+    startSound.volume = volume;
+    ghostSound.volume = volume;
+    sizeSound.volume = volume;
+    reduceGapSound.volume = volume;
+    starSound.volume = volume;
+  }
+
   // Append the button to the body element
   document.body.appendChild(button);
-};
+
+  // Call adjustVolume() initially to set the volume based on the stored state
+  adjustVolume();
+
+  // Additional logic to ensure flapSounds and coinSounds are muted correctly on page reload
+  if (!isSfxOn) {
+    initializeFlapSounds();
+    initializeCoinSounds();
+    adjustVolume();
+  }
+}
+
+createSfxButton();
+
+
 
 function getRandomInt(min, max) {
   const range = max - min + 1;
@@ -1535,8 +1572,6 @@ function playDeathSound() {
 // Load in the buttons and background. Start background animation.
 window.onload = function() {
   drawCollectedCoins();
-  createSfxButton(); 
-  createMusicButton();
   drawBird();
   
   const video = document.getElementById("gameVideo");
